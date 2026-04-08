f471dfcb5164ea748590f44599db8b60aeb8abc3
AVENTRAGOD <aurumstudio101@gmail.com>
2026-04-08 21:49:18 +0530

    Move Vercel config to root repository

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ echo "Installing Flutter directly via Git..."
 git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
+# We must CD into the subfolder because Vercel starts at the root
+cd flare_dating_app || exit 1
+
 echo "Fetching dependencies..."
 flutter pub get
 
